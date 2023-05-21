3a33fdf8c702c54f74223e0ba9b9f99a6adb1bb2
EL Mehdi HASSAR <mido.hass.31@gmail.com>
2023-05-21 17:36:22 +0100

    step-4

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:learningdart/views/login_view.dart';
 import 'package:learningdart/views/register_view.dart';
 import 'package:learningdart/views/verify_email_view.dart';
 import 'Firebase_options.dart';
-import 'dart:developer' as devtools show log;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,6 +16,7 @@ void main() {
       routes: {
         '/login/': (context) => const LoginView(),
         '/register/': (context) => const RegisterView(),
+        '/notes/':(context) => const NotesView(),
       },
     ),
   );
